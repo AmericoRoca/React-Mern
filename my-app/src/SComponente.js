ea0fb14445c21e7faa228ccc1100921c0ac95702
Américo <americochiclana@gmail.com>
2023-06-24 11:16:40 +0200

    Vistas JSX

--- my-app/src/SComponente.js
+++ my-app/src/SComponente.js
@@ -2,16 +2,28 @@ import React from 'react'
 
 export const SComponente = () => {
 
-    const libros = ["Harry Potter", "Clean"]
-  return (
-    <div className='segundoComponente'>
-        <h1>Listado de libros</h1>
-        <ul>
-            <li>
-                { v  }
-            </li>
-        </ul>
+    const libros = ["Harry Potter", "Clean", "otro"];
 
-    </div>
-  )
+    return (
+      
+      <div className='segundoComponente'>
+          <h1>Listado de libros</h1>
+          {libros.length >= 1 ?  (
+            <ul>
+                <li>
+                    { 
+                    
+                      libros.map((libro, indice) => {
+
+                        return <li key={indice}>{libro}</li>;
+                        
+                      })
+                    }
+                </li>
+            </ul>)
+            : (<p>No hay libros</p>)
+          } 
+
+      </div>
+    )
 }
