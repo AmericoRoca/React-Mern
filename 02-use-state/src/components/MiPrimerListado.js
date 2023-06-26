20a574ad8ec761c0532ae9859da5ed7a89983ce2
Américo <americochiclana@gmail.com>
2023-06-26 18:03:52 +0200

    Hook useState

--- 02-use-state/src/components/MiPrimerListado.js
+++ 02-use-state/src/components/MiPrimerListado.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 
 export const MiPrimerListado = () => {
+
+    /* Problematica
 
     let nombre = "Americo Chiclana";
 
     const cambiarNombre = (e) =>{
         nombre = "Paco gomez";
-    }
+    }*/
 
+    const [nombre, setNombre] = useState("Americo Chiclana");
+
+    const cambiarNombre = e =>{
+        setNombre("Paco gomez");
+    }
 
   return (
     <div>
