8e3cde8b093bdc195ab19360df3cd9ec11240e93
Américo <americochiclana@gmail.com>
2023-06-27 11:19:49 +0200

    Ejercicio 1.1

--- 02-use-state/src/ComponenteEjercicio.js
+++ 02-use-state/src/ComponenteEjercicio.js
@@ -1,38 +1,44 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const ComponenteEjercicio = ({ año }) => {
 
+  const [añoActual, setAñoActual] = useState(año);
 
+  const cambiarAño = (e, estado) => {
+    console.log("Funcionando en estado: " + estado);
 
-    const añoAP = (e, estado) => {
-
-        console.log("Funcionando en estado: "+estado);
-
-        if (estado == 1){
-           let añoAnterior = año--;
-           return año;
-            console.log("Funcionando estado 1")
-            console.log(año)
-       
-        } else {
-           let añoProximo = año++;
-            console.log("Funcionando estado 0")
-            console.log(año)
-        }
+    if (estado === 1) {
+      setAñoActual(añoActual - 1);
+      console.log("Funcionando estado 1");
+    } else {
+      setAñoActual(añoActual + 1);
+      console.log("Funcionando estado 0");
     }
+  }
 
-    
+  const cambiarAñoInput = (e, añoActual) =>{
+    setAñoActual(añoActual)
+  }
+
+  
 
   return (
     <div>
-        <h1>Ejercicio 1</h1>
-        <h3>Año Actual: {año}</h3>
-        <button onClick={ e => añoAP(e, 1)}>Año anterior</button>
-        <br/>
-        <button onClick={ e => añoAP(e, 0)}>Año proximo</button>
-        <br/>
-        <input type="text" placeholder="introduce año"/>
+      <h1>Ejercicio 1</h1>
+      <h3>Año Actual: {añoActual}</h3>
+      <button onClick={(e) => cambiarAño(e, 1)}>Año anterior</button>
+      <br />
+      <button onClick={(e) => cambiarAño(e, 0)}>Año próximo</button>
+      <br />
+      <input type="text" placeholder="Introduce año" onKeyUp={ e => cambiarAñoInput(e, e.target.value)}/>
     </div>
-  )
+  );
 }
+
+
+ComponenteEjercicio.propTypes = {
+  año: PropTypes.number.isRequired,
+
+}
+
