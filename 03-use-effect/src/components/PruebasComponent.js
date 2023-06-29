f44151f94c87c95d9064bafd3f8700e4b87ff61a
Américo <americochiclana@gmail.com>
2023-06-29 10:18:52 +0200

    use-effect2

--- 03-use-effect/src/components/PruebasComponent.js
+++ 03-use-effect/src/components/PruebasComponent.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-
+import '../App.css'
 
 export const PruebasComponent = () => {
 
     const [usuario, setUsuario] = useState("Americo Chiclana");
     const [fecha, setFecha] = useState("29-10-1991")
+    const [contador, setContador] = useState(0);
 
     const modUsuario = e =>{
         setUsuario(e.target.value);
@@ -15,12 +16,15 @@ export const PruebasComponent = () => {
     //Con corchetes solo se ejecuta una vez
     useEffect(() =>{
         console.log("Has realizado un cambio")
-    }, []);
+    });
 
     //Se ejecuta solo si cambia el usuario
     useEffect(() =>{
         console.log("Has realizado un cambio")
-    }, [usuario]);
+        setContador(contador+1);
+        console.log("Has cambiado: "+contador)
+    
+    }, [fecha, usuario]);
 
     const cambiarFecha = e =>{
         setFecha(new Date().toLocaleDateString());
@@ -30,7 +34,7 @@ export const PruebasComponent = () => {
   return (
     <div>
         <h1>Efecto use effect</h1>
-        <strong className='label'>{ usuario }</strong>
+        <strong className={ contador >= 10 ? "label label-green" : "label" }>{ usuario }</strong>
         <strong className='label'>{ fecha }</strong>
         <p>
             
