69b3e63251e5b7b338e45296bda50ce57c891674
Américo <americochiclana@gmail.com>
2023-06-29 12:01:21 +0200

    capturar y mostrar errores

--- 03-use-effect/src/components/AjaxComponent.js
+++ 03-use-effect/src/components/AjaxComponent.js
@@ -6,6 +6,7 @@ export const AjaxComponent = () => {
 
     const [usuarios, setUsuarios] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [errores, setErrores] = useState([])
   
     /*const getUsuariosAjaxPms = () => {
       fetch("https://reqres.in/api/users?page=2")
@@ -23,11 +24,18 @@ export const AjaxComponent = () => {
     const getUsuariosAjaxAW = () =>{
 
         setTimeout(async () =>{
-            const peticion = await fetch("https://reqres.in/api/users?page=2");
-            const { data } = await peticion.json();
+            try{
+                const peticion = await fetch("https://reqres.in/api/users?page=2");
+                const { data } = await peticion.json();
+                
+                setUsuarios(data);
+                setLoading(false);
+
+            } catch(error){
+                console.log("Hola"+error.message);
+                setErrores(error.message);
+            }
             
-            setUsuarios(data);
-            setLoading(false);
 
         }, 2000); 
 
@@ -38,8 +46,17 @@ export const AjaxComponent = () => {
       //getUsuariosAjaxPms();
         getUsuariosAjaxAW(); 
     }, []);
+
+    if(errores !== ""){
+          //Cuando esta todo cargando
+          return(
+            <div>
+                 {errores}
+            </div>
+        );
     
-    if(loading == true){
+    
+    } else if(loading === true){
          //Cuando esta todo cargando
         return(
             <div>
@@ -47,7 +64,7 @@ export const AjaxComponent = () => {
             </div>
         );
 
-    } else {
+    } else if(loading === "false" && errores === ""){
         //Cuando todo ha ido bien
         return (
             <div>
@@ -56,7 +73,7 @@ export const AjaxComponent = () => {
                 <ol>
                 {usuarios.map(usuario => (
                     <li key={usuario.id}>
-                        <img src={usuario.avatar} width="80"/>
+                        <img src={usuario.avatar} width="80" alt='imagen-usuario'/>
                         {usuario.first_name} {usuario.last_name}</li>
                 ))}
                 </ol>
