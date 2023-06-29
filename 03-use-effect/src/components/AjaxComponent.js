e46c5a5b8cc1528a0066f3847c0c26546f1d7e57
Américo <americochiclana@gmail.com>
2023-06-29 11:18:34 +0200

    fetch y promise

--- 03-use-effect/src/components/AjaxComponent.js
+++ 03-use-effect/src/components/AjaxComponent.js
@@ -3,50 +3,39 @@ import React, { useEffect, useState } from 'react'
 
 
 export const AjaxComponent = () => {
-
+    
     const [usuarios, setUsuarios] = useState([]);
-
-    //Función generica
-    const getUsuariosEstaticos = () => {
-        setUsuarios([
-            {
-                "id": 7,
-                "email": "michael.lawson@reqres.in",
-                "first_name": "Michael",
-                "last_name": "Lawson",
-                "avatar": "https://reqres.in/img/faces/7-image.jpg"
-            },
-            {
-                "id": 8,
-                "email": "lindsay.ferguson@reqres.in",
-                "first_name": "Lindsay",
-                "last_name": "Ferguson",
-                "avatar": "https://reqres.in/img/faces/8-image.jpg"
-            },
-            {
-                "id": 9,
-                "email": "tobias.funke@reqres.in",
-                "first_name": "Tobias",
-                "last_name": "Funke",
-                "avatar": "https://reqres.in/img/faces/9-image.jpg"
-            }
-        ]);
+    const [isLoading, setIsLoading] = useState(true);
+  
+    const getUsuariosAjaxPms = () => {
+      fetch("https://reqres.in/api/users?page=2")
+        .then(respuesta => respuesta.json())
+        .then((resultado_final => {
+          setUsuarios(resultado_final.data);
+          setIsLoading(false);
+        }))
+        .catch(error => {
+          console.log(error);
+          setIsLoading(false);
+        });
     }
-
+  
     useEffect(() => {
-        getUsuariosEstaticos();
+      getUsuariosAjaxPms();
     }, []);
-
-  return (
-    <div>
+  
+    return (
+      <div>
         <h2>Listado de usuarios via Ajax</h2>
-        <ol className='usuarios'>
-            {
-                usuarios.map(usuario => {
-                    return <li key={usuario.id}>{usuario.first_name} {usuario.last_name}</li>
-                })
-            }
-        </ol>
-    </div>
-  )
-}
+        {isLoading ? (
+          <p>Cargando usuarios...</p>
+        ) : (
+          <ol>
+            {usuarios.map(usuario => (
+              <li key={usuario.id}>{usuario.first_name} {usuario.last_name}</li>
+            ))}
+          </ol>
+        )}
+      </div>
+    );
+  };
