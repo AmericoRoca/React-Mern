493b89d04e3f287f81a12dcad67a0c013a0cbf9c
Américo <americochiclana@gmail.com>
2023-07-12 12:04:24 +0200

    form for movies

--- 06-proyecto1/src/components/Crear.js
+++ 06-proyecto1/src/components/Crear.js
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Crear = () => {
+
+    const tituloComponente = "Añadir pelicula";
+
+    const [ peliState, setPeliState ] = useState(
+       
+    );
+
+    const conseguirDatosForm = e => {
+        e.preventDefault()
+
+        //Conseguir los datos del form
+        let target = e.target;
+        let titulo = target.titulo.value;
+        let descripcion = target.descripcion.value;
+
+        //Crear Objeto de la pelicula a guardar
+        let peli = {
+            id: new Date().getTime(),
+            titulo,
+            descripcion
+        };
+        
+        setPeliState(peli);
+
+    }
+
+
+
   return (
     <div className="add">
-              <h3 className="title">Añadir pelicula</h3>
-              <form action="">
-                  <input type="text" placeholder="titulo"/>
-                  <textarea name="" id="" cols="30" rows="10"></textarea>
-                  <input type="submit" value="Enviar"/>
+              <h3 className="title">{tituloComponente}</h3>
+                
+              <form action="" onSubmit={conseguirDatosForm}>
+                  <input 
+                    type="text" 
+                    name="titulo"
+                    placeholder="Titulo"/>
+                  <textarea 
+                    name="descripcion" 
+                    id="descripcion"
+                    cols="30" 
+                    rows="10">
+
+                    </textarea>
+                  <input 
+                    type="submit" 
+                    value="Enviar"/>
               </form>
     </div>
   )
