a31b02828d9072850e707c5e2fcfcc41a7471599
Américo <americochiclana@gmail.com>
2023-07-14 23:10:35 +0200

    useNavigate

--- 07-router-nav/src/components/Persona.js
+++ 07-router-nav/src/components/Persona.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const Persona = () => {
 
     const {nombre = "Americo", apellido = "Chiclana"} = useParams();
+
+    const navegar = useNavigate();
+
+
+    const enviar = (e) =>{
+        e.preventDefault();
+        let nombre = e.target.nombre.value;
+        let apellido = e.target.apellido.value;
+        let url = `/persona/${nombre}/${apellido}`;
+
+        if(nombre.length <= 0 && apellido.length <= 0){
+            navegar("/inicio")
+        } else {
+            navegar(url);
+        }
+        
+    };
 
 
   return (
     <div>
         <h1>Pagina de persona</h1>
         <h3>{nombre} & {apellido}</h3>
+
+        <form onSubmit={enviar}>
+            <input type='text' name='nombre'></input>
+            <input type='text' name='apellido'></input>
+            <input type='submit' value="enviar" name='enviar'></input>
+        </form>
     </div>
     
   )
